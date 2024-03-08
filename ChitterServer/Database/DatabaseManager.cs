1947a4b451d6dbabac6bca0ecd2d0b80f86330d3
Ellis <me@ellis.pw>
2024-03-08 22:43:40 +0000

    create databasemanager framework

---
+++ ChitterServer/Database/DatabaseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitterServer.Database {
+    internal class DatabaseManager {
+        private readonly string _File_Name;
+
+        internal DatabaseManager( string file_name ) {
+            this._File_Name = file_name;
+        }
+    }
+}
