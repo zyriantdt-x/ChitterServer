c09740a59ec3ba58e30ada490dc399a6b1ec53e1
Ellis <me@ellis.pw>
2024-03-10 16:37:41 +0000

    add exception for chatuser not being found

---
+++ ChitterServer/Chat/Users/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitterServer.Chat.Users {
+    internal class UserNotFoundException : Exception {
+        internal UserNotFoundException( string username )
+            : base( $"Attempt to authenticate '{username}' failed" ) { }
+    }
+}
