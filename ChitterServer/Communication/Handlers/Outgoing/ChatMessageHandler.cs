bdd3129e129fd554ded18f307c1fbf9e39db6f8a
Ellis <me@ellis.pw>
2024-03-10 18:59:39 +0000

    add a chat message handler outgoing

---
+++ ChitterServer/Communication/Handlers/Outgoing/ChatMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitterServer.Communication.Handlers.Outgoing {
+    internal class ChatMessageHandler : OutgoingMessage {
+        internal ChatMessageHandler(DateTime timestamp, string sender_display_name, string message) 
+            : base("CHAT_MESSAGE") {
+            base.AppendObject( "timestamp", timestamp );
+            base.AppendObject( "sender_display_name", sender_display_name );
+            base.AppendObject( "message", message );
+        }
+    }
+}
