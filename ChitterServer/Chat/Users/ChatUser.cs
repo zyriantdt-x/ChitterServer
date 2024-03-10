229be1e07e57432822a669f44999cd824f401a35
Ellis <me@ellis.pw>
2024-03-10 16:37:15 +0000

    added authentication logic to chatuser

--- ChitterServer/Chat/Users/ChatUser.cs
+++ ChitterServer/Chat/Users/ChatUser.cs
@@ -1,19 +1,37 @@
 ﻿using ChitterServer.Communication.Clients;
+using ChitterServer.Database;
+using ChitterServer.Database.Adapters;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChitterServer.Chat.Users {
-    internal class ChatUser {
+    internal class ChatUser : IDisposable {
         private string _Uuid;
         private string _Username;
 
         private CommunicationClient _CommunicationClient;
 
-        internal ChatUser() {
+        internal ChatUser( CommunicationClient communcation_client, DataRow user_data ) {
+            if( communcation_client == null )
+                throw new ArgumentNullException( "communication_client" );
+            this._CommunicationClient = communcation_client;
 
+            if( user_data == null )
+                throw new ArgumentNullException( "user_data" );
+
+            if( user_data[ "uuid" ] == null )
+                throw new MalformedDataException( "uuid" );
+            this._Uuid = Convert.ToString( user_data[ "uuid" ] );
+
+            if( user_data[ "username" ] == null )
+                throw new MalformedDataException( "username" );
+            this._Username = Convert.ToString( user_data[ "username" ] );
+
+            ChitterEnvironment.ChatManager.ChatUserManager.RegisterChatUser( this );
         }
 
         internal static ChatUser TryAuthenticate( CommunicationClient communication_client, string username, string password ) {
@@ -23,16 +41,34 @@ namespace ChitterServer.Chat.Users {
             if( String.IsNullOrWhiteSpace( password ) )
                 throw new ArgumentNullException( password );
 
-            if( username == "ellis" && password == "password" ) {
+            DataRow user_data;
 
-            } else {
-                return null;
+            using( QueryReactor reactor = ChitterEnvironment.DatabaseManager.CreateQueryReactor() ) {
+                reactor.Query = "SELECT * FROM `users` WHERE `username` = @username AND `password` = @password";
+                reactor.AddParameter( "username", username );
+                reactor.AddParameter( "password", password );
+
+                try {
+                    user_data = reactor.Row;
+                } catch( NoDataException ) {
+                    throw new UserNotFoundException( username );
+                } catch( Exception ex ) {
+                    ChatUserManager.Log.Error( $"Unable to authenticate user -> {ex.Message}" );
+
+                    throw;
+                }
             }
 
-            throw new NotImplementedException();
+            return new ChatUser( communication_client, user_data );
         }
 
-        internal string Uuid { get => _Uuid; }
-        internal string Username { get => _Username; }
+        public void Dispose() {
+            ChitterEnvironment.ChatManager.ChatUserManager.DeregisterChatUser( this );
+        }
+
+        internal string Uuid { get => this._Uuid; }
+        internal string Username { get => this._Username; }
+
+        internal CommunicationClient CommunicationClient { get => this._CommunicationClient; }
     }
 }
