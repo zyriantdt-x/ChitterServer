2e33f9429a2f89884292807629ffe0923aeb8f35
Ellis <me@ellis.pw>
2024-03-11 18:01:44 +0000

    fix code formatting

--- ChitterServer/Communication/Handlers/Outgoing/ChatMessageHandler.cs
+++ ChitterServer/Communication/Handlers/Outgoing/ChatMessageHandler.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace ChitterServer.Communication.Handlers.Outgoing {
     internal class ChatMessageHandler : OutgoingMessage {
-        internal ChatMessageHandler(DateTime timestamp, string sender_display_name, string message) 
-            : base("CHAT_MESSAGE") {
+        internal ChatMessageHandler( DateTime timestamp, string sender_display_name, string message )
+            : base( "CHAT_MESSAGE" ) {
             base.AppendObject( "timestamp", timestamp );
             base.AppendObject( "sender_display_name", sender_display_name );
             base.AppendObject( "message", message );
